2e8c0727a0a77cce068e1b77e9dee124b7ead086
张佳奇 <zhangjiaqi@zanzanmd.com>
2017-08-06 20:45:43 +0800

    create master

--- nodejsproxy/index.js
+++
@@ -1,9 +0,0 @@
-var server = require('./server')
-	router = require('./router')
-	handle = require('./requestHandler')
-
-var handler = {}
-
-handler['/proxy'] = handle.proxy
-
-server.start(router.router, handler)
--- nodejsproxy/router.js
+++
@@ -1,3 +0,0 @@
-exports.router = (handle, pathname, req, res) => {
-	typeof handle[pathname] === 'function' ? handle[pathname](req, res) : console.log('no request handlers')
-}
